10cd0cb62147455942e181a4a3a47bbc19e6d12e
merabi91 <merabirusitashvili@yahoo.com>
2022-10-31 02:21:35 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-country = {
+var country = {
     name:"შვეიცარია",
     languages: ["გერმანული" , "ფრანგული" , "იტალიური"],
-    capital:{
-    name:"ბერნი",
-    population:123123
-    },
-    cities :[
-    {name:"ცურიხი", population:31232131},
-    {name:"ჟენევა", population:12313},
-    {name:"რამე", population:13123211231}
+    capital: {
+        name:"ბერნი", 
+        population:123123},
+    cities : [
+        {name:"ცურიხი", population:31232131},
+        {name:"ჟენევა", population:12313},
+        {name:"რამე", population:13123211231}
     ]
-    }
+}
+console.log(country.capital.name)
