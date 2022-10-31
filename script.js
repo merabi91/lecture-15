6e4a3fb41a2239980cab784a97a4731fb471b3cf
merabi91 <merabirusitashvili@yahoo.com>
2022-11-01 00:18:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var country = {
-    name:"შვეიცარია",
-    languages: ["გერმანული" , "ფრანგული" , "იტალიური"],
-    capital: {
-        name:"ბერნი", 
-        population:123123},
-    cities : [
-        {name:"ცურიხი", population:31232131},
-        {name:"ჟენევა", population:12313},
-        {name:"რამე", population:13123211231}
+var cinema = {
+    name:"Cavea Cinemas",
+    languages: ["English" , "Georgian", "Subtitles"],
+    address: ["City Mall" , "East Point" , "Tbilisi Mall" , "Galleria"],
+    movie: {
+        name:"Black Adam",
+        imdb_rating:7.1}
+    other: [
+        {name:"Halloween Ends", imdb_rating:5.0},
+        {name:"Spider-Man: No Way Home", imdb_rating:8.3},
+        {name:"Where the Crawdads Sing", imdb_rating:7.1}
     ]
 }
-console.log(country.capital.name)
+console.log(cinema.movie.name)
