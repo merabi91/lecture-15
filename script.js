5ab2d09fbfc2799cafca6e62ea962eee93fa3e24
merabi91 <merabirusitashvili@yahoo.com>
2022-11-01 00:32:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ var cinema = {
     address: ["City Mall" , "East Point" , "Tbilisi Mall" , "Galleria"],
     movie: {
         name:"Black Adam",
-        imdb_rating:7.1}
-    other: [
-        {name:"Halloween Ends", imdb_rating:5.0},
-        {name:"Spider-Man: No Way Home", imdb_rating:8.3},
-        {name:"Where the Crawdads Sing", imdb_rating:7.1}
+        imdb_rating:7.1},
+    other_movies: [
+        {other_name:"Halloween Ends", other_imdb_rating:5.0},
+        {other_name:"Spider-Man: No Way Home", other_imdb_rating:8.3},
+        {other_name:"Where the Crawdads Sing", other_imdb_rating:7.1}
     ]
 }
-console.log(cinema.movie.name)
+// console.log(cinema.movie.name)
+// console.log(cinema.movie.imdb_rating)
+
+document.write("<h3>address</h3>")
+for (var i=0; i<cinema.address.length; i++) {
+    document.write(cinema.address[i] + "</br>")
+}
